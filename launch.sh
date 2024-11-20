2dfa0e0843613600b87474e919c829c444132c4d
Shubham Kumar <malhotrashubham48@gmail.com>
2024-11-20 22:10:04 +0530

    added config

---
+++ launch.sh
@@ -0,0 +1,7 @@
+kubectl apply -f db-deployment.yaml
+kubectl apply -f backend-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f db-service.yaml
+kubectl apply -f backend-service.yaml
+kubectl apply -f frontend-service.yaml
+
